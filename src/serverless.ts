140d8e4a280db4fcb94518acbfc0db59e7a978b2
Sudhir Kumar <sudhirkumar.k@blackstraw.ai>
2025-02-19 16:00:09 +0530

    vercel app

--- src/serverless.ts
+++ src/serverless.ts
@@ -2,7 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { ValidationPipe } from '@nestjs/common';
 
 const server = express();
